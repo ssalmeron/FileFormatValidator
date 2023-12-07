0be945af472479a694b94390890fbc227e3aed03
Sergio Salmeron <ssalmeron@gmail.com>
2023-12-06 22:10:52 -0600

    Add documentation to TextDoesNotMatchRegexException
    Added comments to the TextDoesNotMatchRegexException class to provide clarity about its purpose. This custom exception is used when a string does not match its corresponding regular expression during text validation procedures.

--- src/main/java/mx/com/salmeron/libraries/fileformatvalidator/validationrules/errors/TextDoesNotMatchRegexException.java
+++ src/main/java/mx/com/salmeron/libraries/fileformatvalidator/validationrules/errors/TextDoesNotMatchRegexException.java
@@ -2,8 +2,13 @@ package mx.com.salmeron.libraries.fileformatvalidator.validationrules.errors;
 
 import java.util.regex.Pattern;
 
+/**
+ * Custom exception class used to indicate that a string does not match a given regular expression.
+ * This exception can be thrown when performing text validation using regular expressions.
+ */
 public class TextDoesNotMatchRegexException extends Exception {
     private static final long serialVersionUID = 3934761815728292966L;
+
 
     public TextDoesNotMatchRegexException(String text, Pattern regex) {
         super("Regex validation failed: " + text + "with: " +regex.pattern());
